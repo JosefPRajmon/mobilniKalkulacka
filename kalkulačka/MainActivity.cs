50493dd086a4fd0f7f53fd3be45dc1c4c874b17c
Josef Rajmon <rajmon.josef1999@itrajmon.cz>
2022-11-30 11:50:55 +0100

    creating menues. layaou abou, feedback, items,table

--- kalkulačka/MainActivity.cs
+++ kalkulačka/MainActivity.cs
@@ -11,6 +11,12 @@ namespace kalkulačka
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity, BottomNavigationView.IOnNavigationItemSelectedListener
     {
+        //layouts
+        RelativeLayout calculateLayout;
+        RelativeLayout aboutLayout;
+        TableLayout tableLayout;
+
+        //text
         TextView textMessage;
 
         protected override void OnCreate(Bundle savedInstanceState)
@@ -19,7 +25,12 @@ namespace kalkulačka
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
- 
+            calculateLayout = FindViewById<RelativeLayout>(Resource.Id.calculateLayout);
+            aboutLayout = FindViewById<RelativeLayout>(Resource.Id.aboutLayout);
+            tableLayout = FindViewById<TableLayout>(Resource.Id.tableLayout);
+            textMessage = FindViewById<TextView>(Resource.Id.message);
+            BottomNavigationView navigation = FindViewById<BottomNavigationView>(Resource.Id.navigation);
+            navigation.SetOnNavigationItemSelectedListener(this);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
@@ -31,11 +42,45 @@ namespace kalkulačka
         {
             switch (item.ItemId)
             {
-                case Resource.Id.navigation_home:
+                case Resource.Id.navigation_calculate:
                     textMessage.SetText(Resource.String.title_home);
+
+                    //leyout control
+                    calculateLayout.Visibility = ViewStates.Visible;  
+                    tableLayout.Visibility = ViewStates.Invisible;
+                    aboutLayout.Visibility = ViewStates.Invisible;
                     return true;
-                case Resource.Id.navigation_dashboard:
-                    textMessage.SetText(Resource.String.title_dashboard);
+                case Resource.Id.navigation_about:
+                    textMessage.SetText(Resource.String.title_about);
+
+                    //leyout control
+                    calculateLayout.Visibility = ViewStates.Invisible;
+                    tableLayout.Visibility = ViewStates.Invisible;
+                    aboutLayout.Visibility = ViewStates.Visible;
+                    return true;
+                case Resource.Id.navigation_table:
+                    textMessage.SetText(Resource.String.title_table);
+
+                    //leyout control
+                     calculateLayout.Visibility = ViewStates.Invisible;
+                     tableLayout.Visibility = ViewStates.Visible;
+                    aboutLayout.Visibility = ViewStates.Invisible;
+                    return true;
+                case Resource.Id.navigation_feedback:
+                    textMessage.SetText(Resource.String.title_feedback);
+
+                    //leyout control
+                    calculateLayout.Visibility = ViewStates.Invisible;
+                    tableLayout.Visibility = ViewStates.Invisible;
+                    aboutLayout.Visibility = ViewStates.Invisible;
+                    return true;
+                case Resource.Id.navigation_myItens:
+                    textMessage.SetText(Resource.String.title_myItems);
+
+                    //leyout control
+                     calculateLayout.Visibility = ViewStates.Invisible;
+                     tableLayout.Visibility = ViewStates.Invisible;
+                    aboutLayout.Visibility = ViewStates.Invisible;
                     return true;
   //              case Resource.Id.navigation_notifications:
   //                  textMessage.SetText(Resource.String.title_notifications);
