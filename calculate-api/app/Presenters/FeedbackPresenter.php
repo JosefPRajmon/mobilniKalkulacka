488a7b64d924acbe3c5964353f9a03b3c4a66336
Josef Rajmon <rajmon.josef1999@itrajmon.cz>
2023-01-13 08:24:09 +0100

    repeat ID

--- calculate-api/app/Presenters/FeedbackPresenter.php
+++ calculate-api/app/Presenters/FeedbackPresenter.php
@@ -1,9 +1,10 @@
 <?php
 declare(strict_types=1);
 
-namespace App\Presenterss;
+namespace App\Presenters;
 
 class FeedbackPresenter extends Nette\Application\UI\Presenter {
+    /** @var \app\Api\FedbackCentr\FeedbackApi @inject */
     public $fedbackApi;
 
     public function actionSave($title, $feedback) {
